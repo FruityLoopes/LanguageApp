70a3e2ee826f44e62dac9de6a4c5c8877137e69c
FruityLoopes <chadandrews07@gmail.com>
2023-09-27 10:23:15 +0200

    Updated spinner to display languages better

--- app/src/main/java/com/example/languageapp/settings_activity.kt
+++ app/src/main/java/com/example/languageapp/settings_activity.kt
@@ -23,8 +23,8 @@ class settings_activity : AppCompatActivity() {
 
         val languageSpinner: Spinner = findViewById(R.id.languageSpinner)
 
-        // Populate the spinner with language options
-        val languages = arrayOf("en", "af", "zh" , "ja" , "ko") // Add your supported languages here
+        // Populate the spinner with language options "English", "Afrikaans", "中国人" , "日本語" , "한국인"
+        val languages = arrayOf("English", "Afrikaans", "中国人" , "日本語" , "한국인") // Add your supported languages here
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         languageSpinner.adapter = adapter
@@ -36,9 +36,28 @@ class settings_activity : AppCompatActivity() {
         }
 
         btnreturn.setOnClickListener(){
-            myPreference.setLoginCount(languages[languageSpinner.selectedItemPosition])
-            startActivity(Intent(this,MainActivity::class.java))
-            finish()
+           if( languages[languageSpinner.selectedItemPosition] == "English"){
+               myPreference.setLoginCount("en")
+               startActivity(Intent(this,MainActivity::class.java))
+               finish()
+           } else if(languages[languageSpinner.selectedItemPosition] == "Afrikaans"){
+               myPreference.setLoginCount("af")
+               startActivity(Intent(this,MainActivity::class.java))
+               finish()
+           } else if( languages[languageSpinner.selectedItemPosition] == "日本語"){
+               myPreference.setLoginCount("ja")
+               startActivity(Intent(this,MainActivity::class.java))
+               finish()
+           } else if(languages[languageSpinner.selectedItemPosition] == "中国人"){
+               myPreference.setLoginCount("zh")
+               startActivity(Intent(this,MainActivity::class.java))
+               finish()
+           } else if(languages[languageSpinner.selectedItemPosition] == "한국인") {
+               myPreference.setLoginCount("ko")
+               startActivity(Intent(this,MainActivity::class.java))
+               finish()
+           }
+
         }
     }
 }
